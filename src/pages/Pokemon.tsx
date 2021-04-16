a693cd093e038b9dde8b4c7de5029a039728c5aa
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-16 10:56:12 -0700

    added name and pic to pokemon; still working on this Pokemon page

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -5,24 +5,44 @@ import { useParams } from 'react-router-dom';
 const Pokemon:React.FC = () => {
 
   let { pokemonId } = useParams<{pokemonId: string}>();
-  let [mon, setMon] = useState<{id: number, num: string, name: string, img: string}>({
-    id: 0,
-    num: '',
-    name: '',
-    img: '',
-    // type: [''],
-    // height: '',
-    // weight: '',
-    // candy: '',
-    // candy_count: 0,
-    // egg: '',
-    // spawn_chance: 0,
-    // avg_spawns: 0,
-    // spawn_time: '',
-    // multipliers: [],
-    // weaknesses: [],
-    // prev_evolution: [],
-    // next_evolution: []
+  let [mon, setMon] = useState<{
+    id: number,
+    num: string,
+    name: string,
+    img: string,
+    type: Array<string>,
+    height: string,
+    weight: string,
+    candy: string,
+    candy_count?: number,
+    egg: string,
+    spawn_chance: number,
+    avg_spawns: number,
+    spawn_time: string,
+    multipliers: Array<number> | null,
+    weaknesses: Array<string>,
+    prev_evolution?: Array<{num:string, name:string}>,
+    next_evolution?: Array<{num:string, name:string}>
+    }>
+    
+    ({
+      id: 0,
+      num: '',
+      name: '',
+      img: '',
+      type: [''],
+      height: '',
+      weight: '',
+      candy: '',
+      candy_count: 0,
+      egg: '',
+      spawn_chance: 0,
+      avg_spawns: 0,
+      spawn_time: '',
+      multipliers: [], // FIXME
+      weaknesses: [''],
+      prev_evolution: [{num: '', name: ''}],
+      next_evolution: [{num: '', name: ''}]
   });
 
   const [pokemon, setPokemon] = useState(pokemonData);
@@ -35,11 +55,17 @@ const Pokemon:React.FC = () => {
   }, [mon, pokemonId]);
 
   return (
-    <div id="dashboard">
+    <div id="pokemon">
 
       <div className="row text-center mt-3">
           <div className="col">
               <h2>{mon.name}</h2>
+          </div>
+      </div>
+
+      <div className="row text-center mt-3">
+          <div className="col">
+              <img src={mon.img} alt={mon.name}/>
           </div>
       </div>
 
